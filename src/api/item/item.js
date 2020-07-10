ecbd3a485694e298392884dd800e2ab609119411
ozmartins <50338986+ozmartins@users.noreply.github.com>
2020-07-10 13:37:14 -0300

    Update item.js

--- src/api/item/item.js
+++ src/api/item/item.js
@@ -1,13 +1,14 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const itemSchema = new mongoose.Schema({    
+const itemSchema = new mongoose.Schema({
+    identificacao: { type: String, required: true },
     descricao: { type: String, required: true },
-    unidade: { type: String, required: true },
     precoVenda: { type: Number, min: 0 },
-    inativo: { type: Boolean },
-    vende: { type: Boolean },
-    compra: { type: Boolean }
+    unidade: { type: String },
+    ativo: { type: Number },
+    vende: { type: Number },
+    compra: { type: Number }
 })
 
 module.exports = restful.model('Item', itemSchema)
